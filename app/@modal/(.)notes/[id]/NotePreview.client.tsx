57f6f30d681c87228e5acfcccac26fdc09f9b433
Dmytro Farbun <dimofarb@gmail.com>
2025-12-11 17:30:53 +0200

    feat: implement full note preview logic with React Query inside modal preview client component

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,19 +1,64 @@
-'use client'
+'use client';
 
-import { useRouter } from "next/navigation"
-import Modal from "@/components/Modal/Modal"
-import NoteDetailsClient from "@/app/notes/[id]/NoteDetails.client"
+import { useRouter } from 'next/navigation';
+import { useQuery } from '@tanstack/react-query';
 
-const NotePreviewClient = () => {
+import { fetchNoteById } from '@/lib/api';
+import css from './NotePreview.module.css';
+import Modal from '@/components/Modal/Modal';
+
+interface NotePreviewClientProps {
+  id: string;
+}
+
+const NotePreviewClient = ({ id }: NotePreviewClientProps) => {
   const router = useRouter();
 
   const handleClose = () => {
     router.back();
   };
 
+  const {
+    data: note,
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ['note', id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  });
+
+  if (isLoading)
+    return <Modal onClose={handleClose}>Loading, please wait...</Modal>;
+  if (isError || !note)
+    return (
+      <Modal onClose={handleClose}>
+        <p>Something went wrong.</p>
+      </Modal>
+    );
+
   return (
     <Modal onClose={handleClose}>
-      <NoteDetailsClient />
+      <button
+        className={css.backBtn}
+        type="button"
+        onClick={handleClose}
+        aria-label="Close modal"
+      >
+        Go back
+      </button>
+      <div className={css.container}>
+        <div className={css.item}>
+          <div className={css.header}>
+            <h2>{note.title}</h2>
+          </div>
+          <p className={css.content}>{note.content}</p>
+          <p className={css.tag}>{note.tag}</p>
+          <p className={css.date}>
+            {new Date(note.createdAt).toLocaleString('uk-UA')}
+          </p>
+        </div>
+      </div>
     </Modal>
   );
 };
